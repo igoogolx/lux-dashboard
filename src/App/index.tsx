1e15928a45e850f2ca4ed633eb609a4b0c9d9771
igoogolx <27353191+igoogolx@users.noreply.github.com>
2022-08-02 08:59:29 -0700

    refactor: remove unused TitleBar

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -24,7 +24,6 @@ import { TRANSLATION_KEY } from "@/i18n/locales/key";
 import i18next from "i18next";
 import WsClient from "isomorphic-ws";
 import { delay } from "@/utils/delay";
-import TitleBar from "@/components/TitleBar";
 import styles from "./index.module.css";
 
 axios.interceptors.response.use(
@@ -96,9 +95,6 @@ export function App(): JSX.Element {
           }}
         />
       )}
-      <div className={styles.header}>
-        <TitleBar />
-      </div>
       <ElevateModal />
       <div className={styles.body}>
         <div className={styles.nav}>
