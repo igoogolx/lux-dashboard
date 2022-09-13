776417899f542a2be7261d123f534a9e840bc70e
igoogolx <27353191+igoogolx@users.noreply.github.com>
2022-09-13 10:22:33 -0700

    feat: get core port from url

--- src/utils/getHubPort.ts
+++ src/utils/getHubPort.ts
@@ -1,6 +1,9 @@
+const getCorePort = () => {
+  const params = new URL(window.location.href).searchParams;
+  return parseInt(params.get("port") as string, 10);
+};
+
 export default function getHubPort() {
   const isDev = process.env.NODE_ENV === "development";
-  return (
-    (isDev ? Number(process.env.HUB_PORT) : window.getCorePort?.()) || 9000
-  );
+  return (isDev ? Number(process.env.HUB_PORT) : getCorePort()) || 9000;
 }
