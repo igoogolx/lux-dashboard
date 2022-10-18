efae3fa19e581fe2b0c6c927ae3a45d3f0ae7b77
igoogolx <27353191+igoogolx@users.noreply.github.com>
2022-10-18 08:49:01 -0700

    refactor: remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ module.exports = {
           content: colors.gray["100"],
         },
         dark: {
-          DEFAULT: "white",
           line: "#dcddde",
           font: "#1e1e1e",
           content: colors.gray["100"],
