5323779bbf8ff56fba26755985a3ce398108298f
igoogolx <igoogolx@gmail.com>
2023-04-18 23:16:22 -0700

    refactor(LocalHttpServer): use EditItemWithDialog

--- src/components/pages/Setting/LocalHttpServer/index.tsx
+++ src/components/pages/Setting/LocalHttpServer/index.tsx
@@ -1,33 +1,18 @@
 import React, { useState } from "react";
 import styles from "@/components/pages/Setting/index.module.css";
 import { TRANSLATION_KEY } from "@/i18n/locales/key";
-import {
-  Button,
-  Card,
-  Dialog,
-  DialogActions,
-  DialogBody,
-  DialogContent,
-  DialogSurface,
-  DialogTitle,
-  DialogTrigger,
-  Input,
-  Label,
-  Switch,
-} from "@fluentui/react-components";
+import { Card, Switch } from "@fluentui/react-components";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, settingSlice } from "@/reducers";
 import { setSetting, SettingRes } from "lux-js-sdk";
 import { notifier } from "@/components/Core";
+import EditItemWithDialog from "@/components/pages/Setting/EditItemWithDialog";
 
 export default function LocalHttpServer() {
   const { t } = useTranslation();
 
   const setting = useSelector<RootState, SettingRes>((state) => state.setting);
-  const [editedValue, setEditedValue] = useState(
-    setting.localServer.http.port.toString()
-  );
 
   const dispatch = useDispatch();
 
@@ -79,54 +64,16 @@ export default function LocalHttpServer() {
               {t(TRANSLATION_KEY.HTTP_SERVER_SWITCH_TOOLTIP)}
             </div>
           </div>
-          <Dialog
-            modalType="modal"
+          <EditItemWithDialog
+            title="Edit the true proxy server"
             open={openModal}
-            onOpenChange={(e, data) => {
-              setOpenModal(data.open);
+            setOpen={setOpenModal}
+            onSubmit={(value) => {
+              onSubmit({ port: +value });
             }}
-          >
-            <DialogTrigger disableButtonEnhancement>
-              <Input
-                type="number"
-                size="medium"
-                className={styles.input}
-                value={setting.localServer.http.port.toString()}
-              />
-            </DialogTrigger>
-            <DialogSurface aria-describedby={undefined}>
-              <DialogBody>
-                <DialogTitle>Edit the true proxy server</DialogTitle>
-                <DialogContent className={styles.dialogBody}>
-                  <Label required htmlFor="true-proxy-server-input">
-                    True proxy server
-                  </Label>
-                  <Input
-                    type="number"
-                    required
-                    id="true-proxy-server-input"
-                    value={editedValue}
-                    onChange={(e) => {
-                      setEditedValue(e.target.value);
-                    }}
-                  />
-                </DialogContent>
-                <DialogActions>
-                  <DialogTrigger disableButtonEnhancement>
-                    <Button appearance="secondary">Close</Button>
-                  </DialogTrigger>
-                  <Button
-                    appearance="primary"
-                    onClick={() => {
-                      onSubmit({ port: +editedValue });
-                    }}
-                  >
-                    Submit
-                  </Button>
-                </DialogActions>
-              </DialogBody>
-            </DialogSurface>
-          </Dialog>
+            label="True proxy server"
+            value={setting.localServer.http.port.toString()}
+          />
         </div>
       )}
     </Card>
